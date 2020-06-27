7322d3377aa0f960fc349bcf5c76283ad6924c3e
ssimon4399 <541151603@qq.com>
2020-06-27 11:37:40 +0800

    .

--- miniprogram/js/main.js
+++ miniprogram/js/main.js
@@ -15,24 +15,24 @@ export default class Main {
   constructor() {
     this.gameStart = new GameStart()
     this.gameStart.GameEnterUI()
-    // this.login()
+    this.login()
   }
 
-  // login() {
-  //   // 获取 openid
-  //   wx.cloud.callFunction({
-  //     name: 'login', 
-  //     success: res => {
-  //       // window.openid = res.result.wx.onBLEPeripheralConnectionStateChanged((result) => {})
-  //       console.log('用户openid:', res.result.openid)
-  //       //保存用户openid
-  //       app.globalData.openid = res.result.openid
-
-  //     },
-  //     fail: err => {
-  //       console.error(
-  //         'get openid failed with error', err)
-  //     }
-  //   })
-  // }
+  login() {
+    // 获取 openid
+    wx.cloud.callFunction({
+      name: 'login', 
+      success: res => {
+        // window.openid = res.result.wx.onBLEPeripheralConnectionStateChanged((result) => {})
+        console.log('用户信息', res.result)
+        //保存用户openid
+        GameGlobal.openid = res.result.userInfo.openId
+        GameGlobal.appId  = res.result.userInfo.appId
+      },
+      fail: err => {
+        console.error(
+          'get openid failed with error', err)
+      }
+    })
+  }
 }
